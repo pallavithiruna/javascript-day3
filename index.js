8b4ca2620c28d250bbc39741720d62785e93e2ed
pallavithiruna <114101268+pallavithiruna@users.noreply.github.com>
2024-02-28 09:23:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-//1.How to compare two JSON have the same properties without order?
-//  a. var obj1={name:"Person 1",age:5};
-//  b. var obj2={age:5,name:"Person 1"};
 
 
-var obj1={name:"Person 1",age:5};
-var obj2={age:5,name:"Person 1"};
-console.log(JSON.stringify(obj1)===JSON.stringify(obj2));
+//Question 1: How to Compare two JSON have the same properties without order
+const obj1 = { 
+    name: 'Person 1', 
+    age: 5 
+}; 
 
+const obj2 = { 
+    age : 5,
+    name: 'Person 1'
+}; 
+
+const haveSameData = function (obj1, obj2) { 
+    const obj1Length = Object.keys(obj1).length; 
+    const obj2Length = Object.keys(obj2).length; 
+
+    if (obj1Length === obj2Length) { 
+        return Object.keys(obj1).every( 
+            key => obj2.hasOwnProperty(key) 
+                && obj2[key] === obj1[key]); 
+    } 
+    return false; 
+} 
+console.log(haveSameData(obj1, obj2));
 
 
 //2.print all country flags
@@ -19,7 +35,15 @@ request.send();
 request.onload=function(){
     var res=JSON.parse(request.response);
     for(var i=0;i<res.length;i++){
-    // console.log((res[i].flags["png"]));    
-    console.log("CountryName:"+(res[i].name["common"])+" , "+"Region:"+(res[i].region)+" , "+"SubRegion:"+(res[i].subregion)+" , "+"Populations:"+(res[i].population)+" , "+"Flags:"+(res[i].flags["png"]));
+     console.log("Flag:"+(res[i].flags["png"]));    
+    console.log("CountryName:"+(res[i].name["common"])+" , "+"Region:"+(res[i].region)+" , "+"SubRegion:"+(res[i].subregion)+" , "+"Populations:"+(res[i].population));
     }
 };
+
+
+
+
+
+
+
+
